f72d03ea9b1dd31450891c589e133e9decd56a22
ccarter <ccarter@6dfe35fe-931f-0410-af5f-a91b034811cd>
2007-04-26 15:04:56 +0000

    Tag:javacall-advanced-mr2-promo-b20

--- implementation/win32/midp/lcd.c
+++ implementation/win32/midp/lcd.c
@@ -1,26 +1,26 @@
 /*
  *
- * Copyright  1990-2007 Sun Microsystems, Inc. All Rights Reserved.
+ * Copyright  1990-2006 Sun Microsystems, Inc. All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version
- * 2 only, as published by the Free Software Foundation.
+ * 2 only, as published by the Free Software Foundation. 
  * 
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License version 2 for more details (a copy is
- * included at /legal/license.txt).
+ * included at /legal/license.txt). 
  * 
  * You should have received a copy of the GNU General Public License
  * version 2 along with this work; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA
+ * 02110-1301 USA 
  * 
  * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa
  * Clara, CA 95054 or visit www.sun.com if you need additional
- * information or have any questions.
+ * information or have any questions. 
  */
 
 /**
@@ -627,13 +627,8 @@ WndProc (HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam) {
                 } 
                 isPaused =!isPaused;
                 break;
-            } else if(VK_HOME == wParam ||
-                      VK_F7 == wParam) {
+            } else if(VK_HOME == wParam) {
                 javanotify_switch_to_ams();
-                break;
-            } else if(VK_END == wParam ||
-                      VK_F8 == wParam) {
-                javanotify_shutdown();
                 break;
             } else if(VK_F4 == wParam) {
                 javanotify_select_foreground_app();
@@ -1247,7 +1242,6 @@ static int mapKey(WPARAM wParam, LPARAM lParam) {
         return JAVACALL_KEY_BACKSPACE;
 
     case VK_HOME:
-    case VK_F7:
 //        return MD_KEY_HOME;
 
     default:
