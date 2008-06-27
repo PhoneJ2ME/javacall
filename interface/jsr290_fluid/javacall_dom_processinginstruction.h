d29d843de64c7634a950a5e1d60e989c1ad1f3bb
IlyaKrupenin <IlyaKrupenin@6dfe35fe-931f-0410-af5f-a91b034811cd>
2008-06-27 10:25:26 +0000

    Branch for CR 6718511

--- interface/jsr290_fluid/javacall_dom_processinginstruction.h
+++ interface/jsr290_fluid/javacall_dom_processinginstruction.h
@@ -50,46 +50,46 @@ extern "C" {
  * the first token following the markup that begins the processing 
  * instruction.
  * 
- * Note: If ret_value_len is less then length of the returned string this function 
- *       has to return with JAVACALL_OUT_OF_MEMORY code and fill ret_value_len 
+ * Note: If retValueLen is less then length of the returned string this function 
+ *       has to return with JAVACALL_OUT_OF_MEMORY code and fill retValueLen 
  *       with actual length of the returned string.
  *
  * @param handle Pointer to the object representing this processinginstruction.
- * @param ret_value The target of this processing instruction.
- * @param ret_value_len Length of the returned string
+ * @param retValue The target of this processing instruction.
+ * @param retValueLen Length of the returned string
  * 
  * @return JAVACALL_OK if all done successfuly,
  *         JAVACALL_OUT_OF_MEMORY if length of the returend string is more then 
- *                                specified in ret_value_len,
+ *                                specified in retValueLen,
  *         JAVACALL_NOT_IMPLEMENTED when the stub was called
  */
 javacall_result
 javacall_dom_processinginstruction_get_target(javacall_handle handle,
-                                              /* OUT */ javacall_utf16_string ret_value,
-                                              /* INOUT */ javacall_uint32* ret_value_len);
+                                              /* OUT */ javacall_utf16_string retValue,
+                                              /* INOUT */ javacall_uint32* retValueLen);
 
 /**
  * Returns the content of this processing instruction. This is from the first non 
  * white space character after the target to the character immediately 
  * preceding the <code>?&gt;</code>.
  * 
- * Note: If ret_value_len is less then length of the returned string this function 
- *       has to return with JAVACALL_OUT_OF_MEMORY code and fill ret_value_len 
+ * Note: If retValueLen is less then length of the returned string this function 
+ *       has to return with JAVACALL_OUT_OF_MEMORY code and fill retValueLen 
  *       with actual length of the returned string.
  *
  * @param handle Pointer to the object representing this processinginstruction.
- * @param ret_value The content of this processing instruction
- * @param ret_value_len Length of the returned string
+ * @param retValue The content of this processing instruction
+ * @param retValueLen Length of the returned string
  * 
  * @return JAVACALL_OK if all done successfuly,
  *         JAVACALL_OUT_OF_MEMORY if length of the returend string is more then 
- *                                specified in ret_value_len,
+ *                                specified in retValueLen,
  *         JAVACALL_NOT_IMPLEMENTED when the stub was called
  */
 javacall_result
 javacall_dom_processinginstruction_get_data(javacall_handle handle,
-                                            /* OUT */ javacall_utf16_string ret_value,
-                                            /* INOUT */ javacall_uint32* ret_value_len);
+                                            /* OUT */ javacall_utf16_string retValue,
+                                            /* INOUT */ javacall_uint32* retValueLen);
 
 /**
  * Sets the content of this processing instruction. This is from the first non 
@@ -98,14 +98,18 @@ javacall_dom_processinginstruction_get_data(javacall_handle handle,
  * 
  * @param handle Pointer to the object representing this processinginstruction.
  * @param data character data to add to the node, may not be NULL
+ * @param exceptionCode Code of the error if function fails; 
+ *                      see javacall_dom_exceptions 
  * 
  * @return JAVACALL_OK if all done successfuly,
- *         JAVACALL_FAIL if NO_MODIFICATION_ALLOWED_ERR occured,
+ *         JAVACALL_FAIL if error occured; in this case exceptionCode has to be 
+ *                                filled,
  *         JAVACALL_NOT_IMPLEMENTED when the stub was called
  */
 javacall_result
 javacall_dom_processinginstruction_set_data(javacall_handle handle,
-                                            javacall_const_utf16_string data);
+                                            javacall_const_utf16_string data,
+                                            /* OUT */ javacall_dom_exceptions* exceptionCode);
 
 /** 
  * Deletes object representing this processinginstruction
